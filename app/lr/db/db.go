75b2545842b4173a7ed7de789b14ca28437be64e
Bo0km4n <pokopenkatuya@gmail.com>
2019-05-01 19:09:23 +0900

    Add mutex map

--- app/lr/db/db.go
+++ app/lr/db/db.go
@@ -2,30 +2,25 @@ package db
 
 import (
 	"fmt"
-
-	"github.com/k0kubun/pp"
+	"sync"
 
 	"github.com/Bo0km4n/claude/app/common/proto"
 )
 
-var memcache map[string]*proto.PeerEntry
+var memcache sync.Map
 
 func InitDB() {
-	memcache = make(map[string]*proto.PeerEntry, 1024)
+	memcache = sync.Map{}
 }
 
 func InsertEntry(key string, value *proto.PeerEntry) {
-	memcache[key] = value
+	memcache.Store(key, value)
 }
 
 func FetchEntry(key string) (*proto.PeerEntry, error) {
-	v, ok := memcache[key]
+	v, ok := memcache.Load(key)
 	if !ok {
 		return nil, fmt.Errorf("Not found key: %s", key)
 	}
-	return v, nil
-}
-
-func Dump() {
-	pp.Println(memcache)
+	return v.(*proto.PeerEntry), nil
 }
