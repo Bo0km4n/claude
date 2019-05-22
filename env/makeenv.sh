4030bc42320e7da45a5dacd9277a83f4d53c14b4
Bo0km4n <pokopenkatuya@gmail.com>
2019-05-22 09:30:21 +0900

    Update env

--- env/makeenv.sh
+++ env/makeenv.sh
@@ -6,5 +6,5 @@ wget https://dl.google.com/go/go1.11.linux-amd64.tar.gz -O ~/tmp/go1.11.linux-am
 sudo tar -xvf ~/tmp/go1.11.linux-amd64.tar.gz -C ~/tmp go
 sudo mv ~/tmp/go /usr/local
 echo "export GOROOT=/usr/local/go" >> ~/.bashrc
-echo "export GOPATH=$HOME/go" >> ~/.bashrc
-echo "export PATH=$GOPATH/bin:$GOROOT/bin:\$PATH" >> ~/.bashrc
+echo "export GOPATH=\$HOME/go" >> ~/.bashrc
+echo "export PATH=\$GOPATH/bin:\$GOROOT/bin:\$PATH" >> ~/.bashrc
