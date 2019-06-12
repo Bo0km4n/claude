7b7327e99542f6e114e84d1049a81807710de677
Bo0km4n <pokopenkatuya@gmail.com>
2019-06-12 18:07:23 +0900

    WIP

---
+++ benchmark/bidirection/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net"
+	"os"
+	"time"
+
+	"github.com/Bo0km4n/claude/lib"
+	"github.com/k0kubun/pp"
+)
+
+var data []byte
+var counter int
+var before time.Time
+
+func main() {
+	withProxy()
+	// withoutProxy()
+}
+
+func withoutProxy() {
+	mode := os.Getenv("MODE")
+	switch mode {
+	case "SERVER":
+		withoutProxyServer()
+	case "CLIENT":
+		withoutProxyClient()
+	}
+}
+
+func withoutProxyServer() {
+	listener, error := net.Listen("tcp", "192.168.10.101:10000")
+	buf := make([]byte, 1024)
+	if error != nil {
+		panic(error)
+	}
+	for {
+		conn, err := listener.Accept()
+		defer conn.Close()
+		if err != nil {
+			panic(err)
+		}
+		conn.Read(buf)
+		pp.Println("READ")
+		conn.Write(buf)
+	}
+}
+
+func withoutProxyClient() {
+	conn, err := net.Dial("tcp", "192.168.10.101:10000")
+	if err != nil {
+		panic(err)
+	}
+	buf := make([]byte, 1024)
+	for {
+		conn.Write(data)
+		conn.Read(buf)
+	}
+}
+
+var (
+	Mode     = flag.String("m", "server", "application mode")
+	DataPath = flag.String("d", "data path", "dummy data file")
+)
+
+func withProxy() {
+	lib.InitConfig()
+	lib.ConnectToLR(os.Args[1])
+
+	switch *Mode {
+	case "server":
+		quit := make(chan struct{})
+		// Wait another peer connect
+		time.Sleep(5)
+
+		conn, err := lib.NewConnection(os.Args[1], dest[:])
+		if err != nil {
+			log.Fatal(err)
+		}
+		conn.RegisterHandler(
+			func(c *lib.Connection, b []byte) error {
+				c.Write(b)
+				counter++
+				if counter == 1 {
+					initTime()
+				} else {
+					newTime, elapsed := calcElpasedTime(before)
+					before = newTime
+					log.Printf("[Term %d] Elapsed Time: %v\n", counter, elapsed)
+				}
+				if counter >= 100 {
+					quit <- struct{}{}
+				}
+				return nil
+			})
+		go conn.Serve()
+		<-quit
+	case "client":
+		dest := lib.DeserializeID(os.Args[2])
+	}
+}
+
+func start(conn *lib.Connection) {
+	conn.Write(data)
+}
+
+func initTime() {
+	before = time.Now()
+}
+
+func calcElpasedTime(before time.Time) (time.Time, time.Duration) {
+	now := time.Now()
+	duration := now.Sub(before)
+	return now, duration
+}
--- go.mod
+++ go.mod
@@ -1,10 +1,24 @@
 module github.com/Bo0km4n/claude
 
+go 1.12
+
 require (
+	github.com/go-sql-driver/mysql v1.4.1
+	github.com/golang/protobuf v1.3.1
 	github.com/google/gopacket v1.1.16
+	github.com/inconshreveable/mousetrap v1.0.0
+	github.com/jinzhu/gorm v1.9.5
+	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a
 	github.com/k0kubun/pp v2.3.0+incompatible
+	github.com/kelseyhightower/envconfig v1.3.0
 	github.com/mattn/go-colorable v0.1.1
 	github.com/mattn/go-isatty v0.0.5
-	github.com/spf13/cobra v0.0.4 // indirect
+	github.com/spf13/cobra v0.0.4
+	github.com/spf13/pflag v1.0.3
+	golang.org/x/net v0.0.0-20190501004415-9ce7a6920f09
 	golang.org/x/sys v0.0.0-20190222171317-cd391775e71e
+	golang.org/x/text v0.3.2
+	google.golang.org/appengine v1.5.0
+	google.golang.org/genproto v0.0.0-20190425155659-357c62f0e4bb
+	google.golang.org/grpc v1.20.1
 )
